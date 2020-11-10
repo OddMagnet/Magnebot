f6316ae8abf6fd9c318e5df615adba14467dc779
OddMagnet <mibruenen@gmail.com>
2020-11-10 15:46:34 +0100

    Add some more test commands for the bot

--- Sources/Magnebot/main.swift
+++ Sources/Magnebot/main.swift
@@ -1,15 +1,28 @@
 import Sword
 
-let bot = Sword(token: "Your bot token here")
+let bot = Sword(token: Secrets.botToken)
 
-bot.editStatus(to: "online", playing: "with Sword!")
+bot.editStatus(to: "online", playing: "with Swords!")
 
 bot.on(.messageCreate) { data in
     let msg = data as! Message
 
-    if msg.content == "!ping" {
-        msg.reply(with: "Pong!")
+    switch msg.content {
+        case "!ping":
+            msg.reply(with: "Pong!")
+            break
+        case "!69":
+            msg.reply(with: "Nice!")
+            break
+        case "!cdog":
+            msg.reply(with: "No, dishwasher and washingmachines are not the same!")
+            break
+        default:
+            break
     }
+//    if msg.content == "!ping" {
+//        msg.reply(with: "Pong!")
+//    }
 }
 
 bot.connect()
