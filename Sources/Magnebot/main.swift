5a80c7a7c4db6d097cdc6b04a053b2c1a6984161
OddMagnet <mibruenen@gmail.com>
2020-11-11 15:28:06 +0100

    More testing

--- Sources/Magnebot/main.swift
+++ Sources/Magnebot/main.swift
@@ -1,8 +1,36 @@
 import Sword
 
-let bot = Sword(token: Secrets.botToken)
+let botOptions = ShieldOptions(
+    prefixes: ["!"]
+    // requirements: <#T##CommandRequirements#>,
+    // willBeCaseSensitive: <#T##Bool#>,
+    // willDefaultHelp: <#T##Bool#>,
+    // willIgnoreBots: <#T##Bool#>
+)
+//let bot = Sword(token: Secrets.botToken)
+let bot = Shield(token: Secrets.botToken,
+                 shieldOptions: botOptions)
 
 bot.editStatus(to: "online", playing: "with Swords!")
+
+bot.register("odd", message: "Magnet")
+bot.register("echo") { msg, args in
+    msg.reply(with: args.joined(separator: ", "))
+}
+bot.register("users") { msg, _ in
+    bot.guilds.forEach({ key, guild in
+        print(guild.name)
+        guild.getMembers(then: { members, error in 
+            print(members)
+            print(error)
+            msg.reply(with: "Users in \(guild.name):")
+            members?.forEach({ member in
+                msg.reply(with: member.user.username ?? "n/a")
+            })
+        })
+        print("---")
+    })
+}
 
 bot.on(.messageCreate) { data in
     let msg = data as! Message
@@ -15,7 +43,11 @@ bot.on(.messageCreate) { data in
             msg.reply(with: "Nice!")
             break
         case "!cdog":
-            msg.reply(with: "No, dishwasher and washingmachines are not the same!")
+            bot.addReaction("🤡", to: msg.id, in: msg.channel.id)
+            msg.reply(with: "No, dishwasher and washingmachines are not the same!") { reply, error in
+                guard let reply = reply else { return }
+                bot.addReaction("🤡", to: reply.id, in: reply.channel.id)
+            }
             break
         default:
             break
